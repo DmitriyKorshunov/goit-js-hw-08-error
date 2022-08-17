bc62d0fa22158dcb3916868e73691d2368b72a92
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-17 17:31:22 +0300

    Зад 1 создание галереи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
 console.log(galleryItems);
+const linkGallery = document.querySelector('.gallery');
+console.log(linkGallery);
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+linkGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+linkGallery.addEventListener('click', makeClickImg);
+
+function makeGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+               </a></li>`;
+    })
+    .join('');
+}
+
+// function makeClickImg(galleryItems) {
+//   galleryItems.preventDefault();
+//   if (galleryItems.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   let gallery = new SimpleLightbox('.gallery a');
+//   gallery.on('show.simplelightbox', function () {
+//     gallery.defaultOptions.captionDelay = 250;
+//   });
+// }
